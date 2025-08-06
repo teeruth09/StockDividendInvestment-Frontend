4a1b42329a2480e8473b9ee483d479f2feba784f
Teeruth Ieowsakulrat <teeruth2546@gmail.com>
2025-08-06 22:44:10 +0700

    add stock template home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import StockDataGrid from "./home/home";
+
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      {/* header / navbar etc. */}
+      <main style={{ flex: 1, padding: '20px' }}>
+        <StockDataGrid />
+      </main>
     </div>
   );
 }
